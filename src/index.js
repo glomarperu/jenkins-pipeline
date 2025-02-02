f7c268f6bb2f9e2008246b7ebb08784afc1e33eb
glomarperu <glomarperu1@gmail.com>
2025-02-02 13:42:56 -0500

    Actualizando docker nuevo cambio

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = (req, res) => {
-    res.send("¡Hola, Jenkins! Este es un proyecto Node.js con CI/CD en Docker");
+    res.send("¡Hola, Jenkins! Este es un proyecto Node.js con CI/CD en Docker nuevo cambio");
 };
